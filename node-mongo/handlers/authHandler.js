8c8c731256ef84f3674a4df92e782b008bba1280
Stefan Gorgevik <stefangorgevik@hotmail.com>
2020-01-08 19:37:49 +0100

    Update authHandler.js

--- node-mongo/handlers/authHandler.js
+++ node-mongo/handlers/authHandler.js
@@ -42,24 +42,21 @@ const login = (req, res) => {
     .then((data) => {
         bcrypt.compare(req.body.password, data.password, function(err, result) {
             if(err) {
-                console.log('first if')
                 return res.status(500).send("Could not compare passwords")
             }
             if(result) {
                 var tokenData = {
                     id: data._id,
                     full_name: `${data.first_name} ${data.first_name}`,
-                    email: data.emal
+                    email: data.email
                 }
                 var token = jwt.sign(tokenData, config.getConfig('jwt').key)
                 return res.status(200).send({jwt: token, first_name: data.first_name, last_name: data.last_name})
             }
-            console.log('not found!')
             return res.status(400).send('Not found!')
         })
     })
     .catch(err => {
-        console.log('second if')
         console.log(err)
         return res.status(500).send("Could not found user!");
     })
